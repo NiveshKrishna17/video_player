e04f82f664e1ff243307b6e66fe07f72834790cd
NiveshKrishna17 <niveshkrish107@gmail.com>
2024-02-18 23:31:30 +0530

    responsive

--- src/Pages/VideoPlayer.jsx
+++ src/Pages/VideoPlayer.jsx
@@ -16,21 +16,21 @@ const VideoPlayer = () => {
 
   return (
     <Fragment>
-      <div className="bg-indigo-800 h-auto w-auto">
-        <h1 className="text-3xl font-[Poppins] text-justify text-white ml-4 mb-4">
+      <div className="bg-indigo-800 h-auto w-auto p-4">
+        <h1 className="text-3xl font-Poppins text-justify text-white mb-4">
           Video Player
         </h1>
-        <div className="flex">
+        <div className="flex flex-col md:flex-row">
           {/* Video player component */}
-          <div className="w-3/4 max-w-screen-lg ml-4">
+          <div className="w-full md:w-3/4 max-w-screen-lg md:ml-4 mb-4 md:mb-0">
             <VideoPlayerComponent videoUrl={currentVideo} />
-            <h4 className="text-3xl text-white flex items-center justify-center">
+            <h4 className="text-2xl md:text-3xl text-white text-center md:text-left">
               {singleData?.title}
             </h4>
           </div>
 
           {/* Playlist side panel component */}
-          <div className="w-1/4">
+          <div className="w-full md:w-1/4 md:ml-4">
             <PlayList
               videos={videoThumbnails}
               onSelectVideo={handleSelectVideo}
